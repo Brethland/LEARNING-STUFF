72bc6e032d569eff775f4ca8a477721d806a1e57
Brethland <ycy1207694344@gmail.com>
2020-04-09 20:00:37 +0800

    Update for subtyping.

---
+++ Coq/Coq34.v
@@ -0,0 +1,10 @@
+(*
+  This is the exercise for <Software Foundations> V2 CH10.
+  Author: Brethland, Early 2020.
+*)
+
+Set Warnings "-notation-overridden,-parsing".
+Require Import Strings.String.
+Add LoadPath "C:\Users\ycy12\Documents\Workspace\LEARNING-STUFF\Coq".
+Load Coq30.
+
