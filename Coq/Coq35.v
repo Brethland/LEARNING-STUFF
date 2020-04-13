5afe295d80b51bdaf2d114e3aad4285d72cdae8b
Brethland <ycy1207694344@gmail.com>
2020-04-14 03:49:26 +0800

    Update

---
+++ Coq/Coq35.v
@@ -0,0 +1,9 @@
+(*
+  This is the exercise for <Software Foundations> V2 CH11.
+  Author: Brethland, Early 2020.
+*)
+
+Require Import Strings.String.
+Add LoadPath "C:\Users\ycy12\Documents\Workspace\LEARNING-STUFF\Coq".
+Load Coq32.
+
