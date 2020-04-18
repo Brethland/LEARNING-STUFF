d3e00a8ac73a1dd3d2794240c494796e9dd5d11e
Brethland <ycy1207694344@gmail.com>
2020-04-18 23:25:09 +0800

    revise

--- C/boring.c
+++ C/boring.c
@@ -1,1 +1,22 @@
 // A Boring Test
+#include <stdio>
+#define fa(x) x/2
+
+int heap[114514], int end;
+
+void insert(int x) {
+    heap[end++] = x;
+    while(x < fa(x) && x >= 1) swap(heap[x], heap[fa(x)]), x = fa(x);
+}
+int pop() {
+    int t = heap[1],now = 1;
+    while(now < end) {
+        heap[now] = heap[now << 1 | 1] > heap[now << 1] ? heap[now << 1] : heap[now << 1 | 1];
+        now = heap[now << 1 | 1] > heap[now << 1] ? now << 1 : now << 1 | 1;
+    }
+    return t;
+}
+
+int main() {
+
+}
